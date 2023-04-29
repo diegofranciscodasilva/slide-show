2de46666f677667f45825db47717a6649bebbb40
Diego Francisco da Silva <105932643+diegofranciscodasilva@users.noreply.github.com>
2023-04-29 08:50:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const images = [
+    { 'id': '1', 'url':'chrono.jpg' },
+    { 'id': '2', 'url':'inuyasha.jpg' },
+    { 'id': '3', 'url':'tenchi.jpg' },
+    { 'id': '4', 'url':'tenjhotenge.jpg' },
+    { 'id': '5', 'url':'yuyuhakusho.jpg' },
+    { 'id': '6', 'url':'ippo.png' },
+]
+
+const containerItems = document.querySelector('#container-items')
+
+const loadImages = (images, container) => {
+    images.forEach(image => {
+        container.innerHTML += 
+        `<div class='item'>
+            <img src='${image.url}'
+        </div>` 
+    })
+}
+
+loadImages(images, containerItems)
+
+let items = document.querySelectorAll('.item')
+
+const previous = () => {
+    containerItems.appendChild(items[0])
+    items = document.querySelectorAll('.item')
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1]
+    containerItems.insertBefore(lastItem, items[0])
+    items = document.querySelectorAll('.item')
+}
+
+document.querySelector('#previous').addEventListener('click', previous)
+document.querySelector('#next').addEventListener('click', next)
